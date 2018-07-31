b491abadf61111f83b7418d2f78b92b4b4f61fd5
renjunxiang <renjunxiang@wondersgroup.com>
2018-07-31 14:52:42 +0800

    。

--- demo_cut.py
+++ demo_cut.py
@@ -10,23 +10,23 @@ params = {
     'model_path':DIR + '/model_cut/'
 }
 
-data_process = Data_process()
-texts_seq, target = data_process.data_transform(len_min=0,
-                                                len_max=20,
-                                                num_words=5000,
-                                                num=100000,
-                                                file='chat')
-
-with open('./model_cut/data_process.pkl', mode='wb') as f:
-    pickle.dump(data_process, f)
-
-train(x=texts_seq,
-      y=target,
-      num_words=data_process.num_words,
-      batchsize=64,
-      epoch=2,
-      max_seq_len=data_process.max_seq_len,
-      **params)
+# data_process = Data_process()
+# texts_seq, target = data_process.data_transform(len_min=0,
+#                                                 len_max=20,
+#                                                 num_words=5000,
+#                                                 num=100000,
+#                                                 file='chat')
+#
+# with open('./model_cut/data_process.pkl', mode='wb') as f:
+#     pickle.dump(data_process, f)
+#
+# train(x=texts_seq,
+#       y=target,
+#       num_words=data_process.num_words,
+#       batchsize=64,
+#       epoch=2,
+#       max_seq_len=data_process.max_seq_len,
+#       **params)
 
 annotate_cut(data_process_path=DIR + '/model_cut/data_process.pkl',
              **params)
